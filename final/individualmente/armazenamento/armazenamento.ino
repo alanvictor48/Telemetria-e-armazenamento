97f8abbb79164ff54bd8363a40a8ead359ae1562
Daniel-Alencar <51214434+Daniel-Alencar@users.noreply.github.com>
2021-09-27 19:22:47 -0300

    Update armazenamento.ino

--- final/individualmente/armazenamento/armazenamento.ino
+++ final/individualmente/armazenamento/armazenamento.ino
@@ -1,9 +1,8 @@
 /*
   The model file will be:
-
   Time, Altitude, Temperature, Acelerometer
   42.1, 42.1, 42.1, 42.1
-  
+
 */
 
 #include <SPI.h>
@@ -16,22 +15,24 @@ File myFile;
 void setup() {
   Serial.begin(9600);
 
+  pinMode(CS_PIN, OUTPUT);
+
   Serial.print("Initializing SD card...");
-  if (!SD.begin(CS_PIN)) {
+  while (!SD.begin(CS_PIN)) {
     Serial.println("Initialization failed!");
-    while(1);
+    //while(1);
   }
   Serial.println("Initialization done.");
 
-  
+
   myFile = SD.open("data.txt", FILE_WRITE);
 
   if(myFile) {
     Serial.print("Writing...");
-    
+
     myFile.println("Time, Altitude, Temperature, Acelerometer");
     myFile.close();
-    
+
     Serial.println("Done.");
   } else {
     Serial.println("Error opening data.txt");
@@ -39,15 +40,18 @@ void setup() {
 }
 
 void writeOnSD(float time, float altitude, float temperature, float acelerometer) {
-  char string[40];
+  //char string[40];
+  String str;
   myFile = SD.open("data.txt", FILE_WRITE);
-  
+
   if(myFile) {
     Serial.print("Writing...");
 
-    sprintf(string, "%.3f, %.3f, %.3f, %.3f\n", time, altitude, temperature, acelerometer);
-    
-    myFile.print(string);
+    //sprintf(string, "%.3lu, %lu, %f, %f, %f\n", time, altitude, temperature, acelerometer, 4, 4/1.0);
+    str = String(time,3) + "," + String(altitude,3) + "," + String(temperature,3) + "," + String(acelerometer,3);
+
+    Serial.println(str);
+    myFile.print(str);
     myFile.close();
 
     Serial.println("Done.");
